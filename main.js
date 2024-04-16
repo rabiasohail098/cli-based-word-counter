0d586c1b69ae00eead710ed1a0613192dea0d32e
Rabia-Sohail <rabiasohail1209@gmail.com>
2024-04-17 00:08:07 +0500

    created cli based word counter

---
+++ main.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt({ name: "sentence",
+    type: "input",
+    message: "Enter your sentence to count the word?",
+});
+const words = answers.sentence.trim().split('  ');
+console.log(words);
+console.log(`Your sentence words count is ${words.length}`);
